5094413d8c616f0d336c2666eeb5d5a899d1ed9c
souravsarkaar1 <souravsantipur9635@gmail.com>
2024-03-14 14:08:32 +0530

    minor bug fiexed

--- hushl-travel-form/src/routes/AllRoutes.jsx
+++ hushl-travel-form/src/routes/AllRoutes.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 // import Home from '../pages/Home/Home'?
-import Forms from '../pages/Froms/Forms'
-import SuccessFormSubmission from '../pages/FromFinal/FromFinal'
-import Dashboard from '../pages/Dashboard/Dashboard'
+import Forms from '../pages/Froms/Forms';
+import SuccessFormSubmission from '../pages/FromFinal/FromFinal';
+import Dashboard from '../pages/Dashboard/Dashboard';
 
 const AllRoutes = () => {
-    return (
-        <div>
-            <Routes>
-                <Route path='/' element={<Dashboard />} />
-                <Route path='/add' element={<Forms />} />
-                <Route path='/thankyou' element={<SuccessFormSubmission />} />
-            </Routes>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/add" element={<Forms />} />
+        <Route path="/thankyou" element={<SuccessFormSubmission />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
